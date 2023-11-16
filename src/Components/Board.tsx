6f3322622c3368fa6d39ed72c7ab50d37329d073
caminobelllo <alstpgml0812@gmail.com>
2023-11-16 22:38:18 +0900

    course 7.12

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -26,9 +26,14 @@ interface IAreaProps {
 
 const Area = styled.div<IAreaProps>`
   background-color: ${(props) =>
-    props.isDraggingOver ? "pink" : props.isDraggingFromThis ? "red" : "blue"};
+    props.isDraggingOver
+      ? "#dfe6e9"
+      : props.isDraggingFromThis
+      ? "#b2bec3"
+      : "transparent"};
   transition: background-color 0.3s ease-in-out;
   flex-grow: 1;
+  padding: 20px;
 `;
 
 interface IBoardProps {
